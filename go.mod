1611c47f7d6743477a3997b7d8f46538bd93b6b8
Nathan Dantas <than_dantas@hotmail.com>
2023-06-20 00:58:06 -0300

    creating asset entity

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/nathandantas/home-broker-go
 
 go 1.20
+
+require github.com/google/uuid v1.3.0
--- internal/market/entity/investor.go
+++ internal/market/entity/investor.go
@@ -1,34 +1,34 @@
 package entity
 
 type Investor struct {
-	ID string
-	Name string
+	ID            string
+	Name          string
 	AssetPosition []*InvestorAssetPosition
 }
 
 func NewInvestor(id string) *Investor {
 	return &Investor{
-		ID: id,
+		ID:            id,
 		AssetPosition: []*InvestorAssetPosition{},
 	}
 }
 
-func (i *Investor) AddAssetPosition(assetPosition *InvestorAssetPosition){
+func (i *Investor) AddAssetPosition(assetPosition *InvestorAssetPosition) {
 	i.AssetPosition = append(i.AssetPosition, assetPosition)
 }
 
-func (i *Investor) UpdateAssetPosition(assetID string, qtdShares int){
-	assetPosition:= i.GetAssetPosition(assetID)
-	if assetPosition == nil{
-		i.AssetPosition = append(i.AssetPosition, NewInvestorAssetPosition(assetID,qtdShares))
-	}else {
-		assetPosition.Shares+=qtdShares
+func (i *Investor) UpdateAssetPosition(assetID string, qtdShares int) {
+	assetPosition := i.GetAssetPosition(assetID)
+	if assetPosition == nil {
+		i.AssetPosition = append(i.AssetPosition, NewInvestorAssetPosition(assetID, qtdShares))
+	} else {
+		assetPosition.Shares += qtdShares
 	}
 }
 
-func (i *Investor) GetAssetPosition(assetID string) *InvestorAssetPosition{
-	for _, assetPosition := range i.AssetPosition{
-		if assetPosition.AssetID == assetID{
+func (i *Investor) GetAssetPosition(assetID string) *InvestorAssetPosition {
+	for _, assetPosition := range i.AssetPosition {
+		if assetPosition.AssetID == assetID {
 			return assetPosition
 		}
 	}
@@ -37,13 +37,12 @@ func (i *Investor) GetAssetPosition(assetID string) *InvestorAssetPosition{
 
 type InvestorAssetPosition struct {
 	AssetID string
-	Shares int
+	Shares  int
 }
 
 func NewInvestorAssetPosition(assetID string, shares int) *InvestorAssetPosition {
 	return &InvestorAssetPosition{
 		AssetID: assetID,
-		Shares: shares,
+		Shares:  shares,
 	}
 }
-
